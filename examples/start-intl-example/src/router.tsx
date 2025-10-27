81985e046f5ff74a490fae5f6cb0eb654c0c797a
daveycodez <daveycodez@gmail.com>
2025-10-26 21:32:28 -0700

    Add locale-based URL rewriting to router
    Introduces a rewrite configuration to the router that redirects the root path '/' to the default locale path using the routing configuration. This ensures users are directed to the appropriate localized route by default.

--- examples/start-intl-example/src/router.tsx
+++ examples/start-intl-example/src/router.tsx
@@ -1,5 +1,5 @@
 import { createRouter } from "@tanstack/react-router"
-
+import { routing } from "./i18n/routing"
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen"
 
@@ -8,6 +8,17 @@ export const getRouter = () => {
     return createRouter({
         routeTree,
         scrollRestoration: true,
-        defaultPreloadStaleTime: 0
+        defaultPreloadStaleTime: 0,
+        rewrite: {
+            input: ({ url }) => {
+                if (url.pathname === "/") {
+                    url.pathname = `/${routing.defaultLocale}`
+                }
+                return url
+            },
+            output: ({ url }) => {
+                return url
+            }
+        }
     })
 }
