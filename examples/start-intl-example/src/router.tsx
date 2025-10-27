3f44b1d4907d71c84e8848d83aeb48f95ea4b1f6
daveycodez <daveycodez@gmail.com>
2025-10-26 21:40:00 -0700

    Add locale detection to router rewrite logic
    Introduces isomorphic locale detection using the Accept-Language header on the server and navigator.language on the client. The router now rewrites the root path to use the detected locale instead of always defaulting to the preset locale.

--- examples/start-intl-example/src/router.tsx
+++ examples/start-intl-example/src/router.tsx
@@ -1,7 +1,19 @@
 import { createRouter } from "@tanstack/react-router"
+import { createIsomorphicFn } from "@tanstack/react-start"
+import { getRequestHeader } from "@tanstack/react-start/server"
 import { routing } from "./i18n/routing"
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen"
+
+const getAcceptLanguage = createIsomorphicFn()
+    .server(() => {
+        const acceptLanguage = getRequestHeader("accept-language")
+        const locale = acceptLanguage?.split("-")[0]
+        return locale
+    })
+    .client(() => {
+        return navigator.language
+    })
 
 // Create a new router instance
 export const getRouter = () => {
@@ -11,8 +23,12 @@ export const getRouter = () => {
         defaultPreloadStaleTime: 0,
         rewrite: {
             input: ({ url }) => {
+                const acceptLanguage = getAcceptLanguage()
+                const locale =
+                    acceptLanguage?.split("-")[0] || routing.defaultLocale
+
                 if (url.pathname === "/") {
-                    url.pathname = `/${routing.defaultLocale}`
+                    url.pathname = `/${locale}`
                 }
                 return url
             },
