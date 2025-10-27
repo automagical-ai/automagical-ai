a8544a01aa914493f1499d00e52af9dd7a13c987
daveycodez <daveycodez@gmail.com>
2025-10-26 20:14:54 -0700

    Switch start-intl to tsup, update build config
    Replaced tsc with tsup for building the start-intl package, added a tsup config file, updated tsconfig target/module, and set up the package for local development. Updated dependencies and lockfile to reflect these changes.

---
+++ packages/start-intl/tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+	entry: ["src/index.ts"],
+	format: ["esm"],
+	dts: true,
+	sourcemap: true,
+	clean: true,
+	external: [
+		"react",
+		"react-dom",
+		"use-intl",
+		"@tanstack/react-router",
+		"@tanstack/react-start",
+		"@tanstack/router"
+	],
+	treeshake: true,
+	splitting: false
+})
+
