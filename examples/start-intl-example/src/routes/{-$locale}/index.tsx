f36233c73d9254f26a170b73a4b5be04e9a30d4d
daveycodez <daveycodez@gmail.com>
2025-10-28 00:44:15 -0700

    Enable loader and display messages in Home route
    Uncommented and enabled the loader function in the Home route to fetch translated messages and server function messages. Updated the Home component to display these messages using Route.useLoaderData().

--- examples/start-intl-example/src/routes/{-$locale}/index.tsx
+++ examples/start-intl-example/src/routes/{-$locale}/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router"
 import { createServerFn } from "@tanstack/react-start"
 import type { Locale } from "use-intl"
-import { useTranslations } from "use-intl"
+import { createTranslator, useTranslations } from "use-intl"
 import { getTranslator } from "@/i18n/messages"
 
 const getServerMessage = createServerFn()
@@ -16,31 +16,31 @@ const getServerMessage = createServerFn()
     })
 
 export const Route = createFileRoute("/{-$locale}/")({
-    component: Home
-    // loader: async ({ context: { locale, messages } }) => {
-    //     const t = createTranslator({
-    //         locale,
-    //         messages
-    //     })
+    component: Home,
+    loader: async ({ context: { locale, messages } }) => {
+        const t = createTranslator({
+            locale,
+            messages
+        })
 
-    //     return {
-    //         messageFromLoader: t("example_message", { username: "John Doe" }),
-    //         serverFunctionMessage: await getServerMessage({
-    //             data: { emoji: "📩", locale }
-    //         })
-    //     }
-    // }
+        return {
+            messageFromLoader: t("example_message", { username: "John Doe" }),
+            serverFunctionMessage: await getServerMessage({
+                data: { emoji: "📩", locale }
+            })
+        }
+    }
 })
 
 function Home() {
     const t = useTranslations()
-    // const { serverFunctionMessage, messageFromLoader } = Route.useLoaderData()
+    const { serverFunctionMessage, messageFromLoader } = Route.useLoaderData()
 
     return (
         <div className="p-2">
-            {/* <h2>Message from loader: {messageFromLoader}</h2>
+            <h2>Message from loader: {messageFromLoader}</h2>
 
-            <h2>Server function message: {serverFunctionMessage}:</h2> */}
+            <h2>Server function message: {serverFunctionMessage}:</h2>
 
             <h2>{t("example_message", { username: "John Doe" })}</h2>
         </div>
